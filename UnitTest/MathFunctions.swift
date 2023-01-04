a4cf9ddebedadbec26b60d0c98e6680ea1e9fd6a
OfficialJamalJones <jamalamanjones@gmail.com>
2023-01-04 13:40:33 -0500

    Added Tests

---
+++ UnitTest/MathFunctions.swift
@@ -0,0 +1,24 @@
+//
+//  MathFunctions.swift
+//  UnitTest
+//
+//  Created by Consultant on 1/4/23.
+//
+
+import Foundation
+
+public class MathFunctions {
+    
+    static func addNumbers(x: Int, y: Int) -> Int {
+        return x + y
+    }
+    
+    static func multiplyNumbers(x: Int, y: Int) -> Int {
+        return x * y
+    }
+    
+    static func divideNumbers(x: Int, y: Int) -> Int {
+        return x / y
+    }
+    
+}
--- UnitTest/ViewController.swift
+++ UnitTest/ViewController.swift
@@ -5,6 +5,12 @@
 //  Created by Consultant on 1/4/23.
 //
 
+//Task : Re-define what a “test” is, for assignments.
+
+//1. Why do we need unit testing? Unit testing ensures that all code meets quality standards before it's deployed. This ensures a reliable engineering environment where quality is paramount.
+
+//2. What are the benefits of unit tests? Over the course of the product development life cycle, unit testing saves time and money, and helps developers write better code, more efficiently.
+
 import UIKit
 
 class ViewController: UIViewController {
